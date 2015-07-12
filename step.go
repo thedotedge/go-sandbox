1c9d5ed02089dcc7dd92de06461ec4068d4f7f12
thedotedge <thedotedge@gmail.com>
2015-07-12 19:50:29 +0200

    first drop

---
+++ step.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("My favorite number is ", rand.Intn(10))
+	fmt.Println("The time is ", time.Now())
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+	_, y, _ := split(17)
+	fmt.Println(y)
+}
+
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+
+func split(sum int) (x, y, z int) {
+	x = sum * 4 / 9
+	y = sum - x
+	z = sum + x
+	return
+}
